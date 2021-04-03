7ad71fbac8c385b1b3671ec1df25b32f4a3c7ad4
Monish <monishsatam96@gmail.com>
2021-04-03 14:48:31 +0530

    add alt local route

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class CommentForm(FlaskForm):
     submit=SubmitField('submit')
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
